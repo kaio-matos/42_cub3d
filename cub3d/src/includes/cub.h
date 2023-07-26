8b2bd173162435794863e9cf7d2eb6dc0e200dfe
Kaio Matos <kaio_matos22@hotmail.com>
2023-07-26 20:02:41 -0300

    setup files

---
+++ cub3d/src/includes/cub.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmatos-s <kmatos-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/26 19:51:43 by kmatos-s          #+#    #+#             */
+/*   Updated: 2023/07/26 19:55:21 by kmatos-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+/**
+	Allowed Functions:
+
+	open, close, read, write,
+	printf, malloc, free, perror,
+	strerror, exit
+
+	- All functions of the math
+	library (-lm man man 3 math)
+
+	- All functions of the MinilibX
+*/
---
+++ cub3d/src/main.c
@@ -0,0 +1,7 @@
+#include <cub.h>
+
+int	main(void)
+{
+	printf("Olá mundo!");
+	return (0);
+}
