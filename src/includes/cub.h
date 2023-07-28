5a7e54a5674deb6588c9ac3d49f5513f2dabec65
Kaio Matos <kaio_matos22@hotmail.com>
2023-07-27 22:05:35 -0300

    wip

--- src/includes/cub.h
+++ src/includes/cub.h
@@ -6,7 +6,7 @@
 /*   By: kmatos-s <kmatos-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 19:51:43 by kmatos-s          #+#    #+#             */
-/*   Updated: 2023/07/26 22:07:22 by kmatos-s         ###   ########.fr       */
+/*   Updated: 2023/07/27 21:54:34 by kmatos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,10 @@
 */
 
 # define WINDOW_X 16777216
+# define MAP_WIDTH 24
+# define MAP_HEIGHT 24
+# define SCREEN_WIDTH 640
+# define SCREEN_HEIGHT 480
 
 typedef struct s_window
 {
@@ -38,6 +42,18 @@ typedef struct s_window
 	int		width;
 	int		height;
 }	t_window;
+
+typedef struct s_posi
+{
+	int	x;
+	int	y;
+}	t_posi;
+
+typedef struct s_posd
+{
+	double	x;
+	double	y;
+}	t_posd;
 
 /******************************************************************************\
 * WINDOW																	   *
@@ -48,6 +64,6 @@ void		w__init(int width, int height);
 void		w__open(void);
 int			w__close(void);
 
-int			w__render(void);
+int			w__render(int world_map[MAP_HEIGHT][MAP_WIDTH]);
 
 #endif
--- src/window/w__render.c
+++ src/window/w__render.c
@@ -1,14 +1,59 @@
 #include <cub.h>
 
-int	w__render(void)
+static int	renderer(t_posd pos, t_posd dir, t_posd plane, int x)
 {
-	static unsigned int	timer;
+	double	camera_x;
+	t_posd	ray_dir;
+	t_posi	map; // integer map position
+	t_posd	side_dist; // side distance
+	t_posd	delta_dist; // delta distance
+	t_posd	perp_dist; // perpendicular distance
+	t_posi	step;
+	int		has_hit_wall;
+	int		side; // was a NS or a EW wall hit
 
-	timer += 1;
-	if (timer <= 120)
-		return (0);
-	if (!w())
-		return (1);
-	timer = 0;
+	camera_x = 2 * x / MAP_WIDTH - 1;
+	ray_dir.x = dir.x * plane.x * camera_x;
+	ray_dir.y = dir.y * plane.y * camera_x;
+
+	map.x = pos.x;
+	map.y = pos.y;
+
+	if (ray_dir.x == 0)
+		delta_dist.x = 1e30;
+	else
+		delta_dist.x = fabs(1 / ray_dir.x);
+	if (ray_dir.y == 0)
+		delta_dist.y = 1e30;
+	else
+		delta_dist.y = fabs(1 / ray_dir.y);
+
+	has_hit_wall = 0;
+
+}
+
+int	w__render(int world_map[MAP_HEIGHT][MAP_WIDTH])
+{
+	int		x;
+	t_posd	pos;
+	t_posd	dir;
+	t_posd	plane;
+
+	x = 0;
+	pos.x = 22;
+	pos.y = 12;
+
+	dir.x = -1;
+	dir.y = 0;
+
+	plane.x = 0;
+	plane.y = 0.66;
+
+	while (x < MAP_WIDTH)
+	{
+		renderer(pos, dir, plane, x);
+		x++;
+	}
+
 	return (0);
 }
